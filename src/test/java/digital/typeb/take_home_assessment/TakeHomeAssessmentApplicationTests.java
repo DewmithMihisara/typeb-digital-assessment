016b155c781268ae3634d4fe17aacb54eb3fbf76
DewmithMihisara <mihisaralokuhewage@gmail.com>
2026-07-02 16:57:34 +0530

    first commit

---
+++ src/test/java/digital/typeb/take_home_assessment/TakeHomeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.typeb.take_home_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
